a16e4c34f1cabe9c9715e2d26da7ca4f098522a0
DariaGazkaeva <d.gazkaeva@gmail.com>
2022-11-05 19:27:26 +0100

    Divided UserRepository into interface and implementation

---
+++ src/main/java/ru/kpfu/itis/dariagazkaeva/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package ru.kpfu.itis.dariagazkaeva.repositories;
+
+import ru.kpfu.itis.dariagazkaeva.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+
+    List<User> findAll();
+
+    boolean save(User user);
+
+    Optional<User> findById(Long id);
+
+    void update(User user);
+
+    void delete(Long id);
+
+}
--- src/main/java/ru/kpfu/itis/dariagazkaeva/repositories/UserRepositoryImpl.java
+++ src/main/java/ru/kpfu/itis/dariagazkaeva/repositories/UserRepositoryImpl.java
@@ -1,19 +1,67 @@
 package ru.kpfu.itis.dariagazkaeva.repositories;
 
-import ru.kpfu.itis.dariagazkaeva.dto.RegisterDto;
 import ru.kpfu.itis.dariagazkaeva.models.User;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
-public class UserRepository {
+public class UserRepositoryImpl implements UserRepository {
 
     private DataSource dataSource;
 
-    public UserRepository(DataSource dataSource) {
+    public UserRepositoryImpl(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public User save(RegisterDto dto) {
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public boolean save(User user) {
+        try(PreparedStatement statement = dataSource.getConnection()
+                .prepareStatement("INSERT INTO public.user(email, password, name) VALUES (?, ?, ?) RETURNING id")
+        )
+        {
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getName());
+
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                user.setId(result.getLong("id"));
+                return true;
+            }
+
+            return false;
+
+        } catch (SQLException e) {
+            //TODO проверить, что это исключение о нарушении уникальности, а не что-то другое, иначе выбросить исключение
+
+            return false;
+        }
+
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
 
     }
 }
