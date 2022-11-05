e3f9be6dc624d979ff784fe2333e86bc65d65c8e
DariaGazkaeva <d.gazkaeva@gmail.com>
2022-11-05 19:39:58 +0100

    Implement registration

--- src/main/java/ru/kpfu/itis/dariagazkaeva/servlets/RegisterServlet.java
+++ src/main/java/ru/kpfu/itis/dariagazkaeva/servlets/RegisterServlet.java
@@ -1,17 +1,76 @@
-package ru.kpfu.itis.dariagazkaeva.servlet;
+package ru.kpfu.itis.dariagazkaeva.servlets;
 
+import ru.kpfu.itis.dariagazkaeva.models.User;
+import ru.kpfu.itis.dariagazkaeva.repositories.UserRepository;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
+
+    // TODO сделать хэширование пароля
+
+    private UserRepository userRepository;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext context = getServletContext();
+        this.userRepository = (UserRepository) context.getAttribute("userRepository");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         getServletContext().getRequestDispatcher("/WEB-INF/views/register.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+
+        List<String> errors = validateRegisterFields(email, password, name);
+
+        if (errors.isEmpty()) {
+            User user = new User(email, password, name);
+            if (userRepository.save(user)) {
+
+                req.getSession().setAttribute("user", user);
+
+                resp.sendRedirect(getServletContext().getContextPath());
+                return;
+            }
+            errors.add("Пользователь с таким EMAIL уже существует");
+
+        }
+        req.setAttribute("errors", errors);
+        getServletContext().getRequestDispatcher("/WEB-INF/views/register.jsp").forward(req, resp);
+    }
+
+    private List<String> validateRegisterFields(String email, String password, String name) {
+
+        List<String> errors = new ArrayList<>();
+
+        if (name == null || name.isEmpty()) {
+            errors.add("Поле ИМЯ должно быть заполнено");
+        }
+        if (email == null || email.isEmpty()) {
+            errors.add("Поле EMAIL должно быть заполнено");
+        }
+        if (password == null || password.isEmpty()) {
+            errors.add("Поле ПАРОЛЬ должно быть заполнено");
+        }
+
+        return errors;
     }
 }
