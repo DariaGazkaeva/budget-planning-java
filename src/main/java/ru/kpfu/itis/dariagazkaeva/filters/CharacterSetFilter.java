a488260beca0892be848000024b26fe76d33d501
DariaGazkaeva <d.gazkaeva@gmail.com>
2022-11-10 17:55:26 +0000

    Fix russian encoding

---
+++ src/main/java/ru/kpfu/itis/dariagazkaeva/filters/CharacterSetFilter.java
@@ -0,0 +1,19 @@
+package ru.kpfu.itis.dariagazkaeva.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("*")
+public class CharacterSetFilter extends HttpFilter {
+    @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+}
